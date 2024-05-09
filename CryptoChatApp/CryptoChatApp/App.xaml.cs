2d15bec10444d9f4300e928031e6a2a0d16e2790
Belachkin <belkin.job123@gmail.com>
2024-05-09 20:17:01 +0300

    init project

---
+++ CryptoChatApp/CryptoChatApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoChatApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CryptoChatApp/CryptoChatApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoChatApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
